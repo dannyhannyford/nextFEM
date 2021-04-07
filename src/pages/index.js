4d5827e5f6a2ef17a47e8db887de0c1e1329b8f9
dannyhannyford <dannyhannyford@gmail.com>
2021-04-07 08:14:55 -0700

    laptop

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import Link from 'next/link'
 
+export const greeting = React.createContext('hello');
 const Home = () => (
-<>
-  <div>index path</div>
-  <Link href="/notes">
-    <a>
-      Link
-    </a>
-  </Link>
-</>
+  <>
+    <div>index path</div>
+    <Link href="/notes">
+      <a>
+        Link
+      </a>
+    </Link>
+  </>
 )
 
 export default Home;
--- src/pages/notes/index.js
+++ src/pages/notes/index.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import greeting from '../index'
 import Link from 'next/link'
 
-const Page = () => (
+const Page = () => {
+  const value = useContext(greeting);
+  console.log('index.js -- value:', value);
+  return (
   <>
     <h1>Note index path</h1>
     <Link href="/notes/[id]" as={`/notes/1`}>
@@ -11,5 +15,6 @@ const Page = () => (
     </Link>
   </>
 )
+  }
 
 export default Page
